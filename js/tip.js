f5bb355e007131e940de733b632051d083ce816c
Sachin Jadhav <jadhav.sachin1993@gmail.com>
2021-11-26 19:57:31 +0530

    Finished 99% of active-states css styles for inputs and buttons

--- js/tip.js
+++ js/tip.js
@@ -29,7 +29,36 @@ for(i = 0; i < noOfButtons; i++) {
 
 document.getElementById('tip__custom').addEventListener('change', tipCalculate, true);
 
+var inp1 = document.getElementById("people-no");
+var inp2 = document.getElementById("bill__amount");
+var inp3 = document.getElementById("tip__custom");
 
+inp1.onkeyup = function (e) {
+    if (inp1.value == 0) {
+        e.target.style.outline = "none";
+        e.target.style.border = "2px solid #b57f75";
+    } else if (inp1.value != 0) {
+        e.target.style.outline = "none";
+        e.target.style.border = "2px solid #26c0ab";
+    }
+};
 
+inp2.onkeyup = function (e) {
+    if (inp2.value == 0) {
+        e.target.style.outline = "none";
+        e.target.style.border = "2px solid #b57f75";
+    } else if (inp2.value != 0) {
+        e.target.style.outline = "none";
+        e.target.style.border = "2px solid #26c0ab";
+    }
+};
 
-
+inp3.onkeyup = function (e) {
+    if (inp3.value == 0) {
+        e.target.style.outline = "none";
+        e.target.style.border = "2px solid #b57f75";
+    } else if (inp3.value != 0) {
+        e.target.style.outline = "none";
+        e.target.style.border = "2px solid #26c0ab";
+    }
+};
